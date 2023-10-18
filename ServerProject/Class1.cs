4eff72d8d7da473a0b52ab916079ad93be16f7e9
IftikharZahid <RaiZahidN@gmail.com>
2023-10-19 01:23:24 +0500

    first commit

---
+++ ServerProject/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ServerProject;
+
+public class Class1
+{
+
+}
---
+++ ServerProject/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Server is running...");
+        Console.ReadLine(); // Add this to keep the console application running
+    }
+}
